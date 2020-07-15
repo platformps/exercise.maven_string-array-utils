b851f93ee93ed88828ecaba42ebdae424a6edd7c
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-15 15:38:55 -0400

    Pangramic done, but will refactor

--- src/main/java/com/github/perscholas/StringArrayUtils.java
+++ src/main/java/com/github/perscholas/StringArrayUtils.java
@@ -1,5 +1,9 @@
 package com.github.perscholas;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -33,7 +37,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
 
     /**
@@ -42,7 +46,17 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+        boolean found = false;
+        int position = 0;
+        while(found == false && position < array.length){
+            if(array[position].contains(value)){
+                found = true;
+            }
+            else{
+                position++;
+            }
+        }
+        return found;
     }
 
     /**
@@ -50,7 +64,14 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] reverseArray = new String[array.length];
+        int reverseArrayPostion = 0;
+
+        for(int count = array.length - 1; count > -1; count--){
+            reverseArray[reverseArrayPostion] = array[count];
+            reverseArrayPostion++;
+        }
+        return reverseArray;
     }
 
     /**
@@ -58,7 +79,7 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static Boolean isPalindromic(String[] array) {
-        return null;
+       return Arrays.equals(array, reverse(array));
     }
 
     /**
@@ -66,7 +87,19 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static Boolean isPangramic(String[] array) {
-        return null;
+        String alphabetList = "abcdefghijklmnopqrstuvwxyz";
+        int postion = 0;
+        boolean arrayIsStillPangramic = true;
+        while(arrayIsStillPangramic && postion < alphabetList.length()){
+            //Gross
+            if(contains(array, String.valueOf(alphabetList.charAt(postion))) || contains(array, String.valueOf(Character.toUpperCase(alphabetList.charAt(postion))))){
+                postion++;
+            }
+            else{
+                arrayIsStillPangramic = false;
+            }
+        }
+        return arrayIsStillPangramic;
     }
 
     /**
@@ -75,7 +108,13 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int occurrences = 0;
+        for(String word : array){
+            if(word == value){
+                occurrences++;
+            }
+        }
+        return occurrences;
     }
 
     /**
@@ -84,7 +123,14 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        int removeValues = getNumberOfOccurrences(array, valueToRemove);
+        String[] returnArray = new String[array.length - removeValues]; // will have the length of the array without the removed values
+        for(int pos = 0; pos < array.length - 1; pos++){
+            if(array[pos].equals(valueToRemove)) {
+                continue;
+            }
+        }
+        return returnArray;
     }
 
     /**
@@ -100,8 +146,8 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
+        StringBuilder sb = new StringBuilder("");
         return null;
     }
-
 
 }

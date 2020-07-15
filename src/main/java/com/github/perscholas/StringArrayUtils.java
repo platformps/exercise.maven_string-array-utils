3bd8edb5a2060962627959e4a34a4a66670e8906
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-15 10:40:10 -0400

    Inital work in stringarrayUtilis

--- src/main/java/com/github/perscholas/StringArrayUtils.java
+++ src/main/java/com/github/perscholas/StringArrayUtils.java
@@ -9,7 +9,7 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return null;
+        return array[0];
     }
 
     /**
@@ -17,7 +17,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return null;
+        return array[1];
     }
 
     /**
@@ -25,7 +25,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length - 1];
     }
 
     /**
